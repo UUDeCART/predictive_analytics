7a4cb47a29035e436f6abb48f7dd24b5b4bdbdb3
Andrew Redd <amredd@gmail.com>
2017-07-26 15:42:32 +0000

    updated Introduction to R and helper scripts.

--- sift.R
+++ sift.R
@@ -1,30 +1,16 @@
-<!DOCTYPE HTML>
-<html>
-
-<head>
-  <meta charset="utf-8">
-  <title>sift.R</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-</head>
-
-<body>
-  <style type="text/css">
-    html, body, #container {
-      height: 100%;
-    }
-    body, #container {
-      overflow: hidden;
-      margin: 0;
-    }
-    #iframe {
-      width: 100%;
-      height: 100%;
-      border: none;
-    }
-  </style>
-  <div id="container">
-    <iframe id="iframe" sandbox="allow-scripts" src="/user/halpo/files/sift.R"></iframe>
-  </div>
-</body>
-
-</html>
+sift <- 
+function( x                 #< A character vector 
+        , pat               #< Regular expression pattern
+        , value=TRUE        #< Return value or indices.
+        , perl=TRUE         #< is `pat` perl compatible?
+        , ignore.case=TRUE  #< Should case be ignored?
+        , ...               #< passed on to <grep>
+        ){
+    #' Pipe compatible regular expression filtering.
+    unique(grep( pattern=pat, x=x
+               , value=value, ignore.case=ignore.case, perl=perl
+               , ...))
+    #' @seealso <grep>.
+    #' @return A subset of x vector that matches the pattern given in `pat`.
+}
+sieve <- function(...)sift(..., invert=TRUE)
